0036b676104f7824787db288a935316a50301f61
Yoshiya Hinosawa <stibium121@gmail.com>
2014-05-13 17:47:00 +0900

    Add coverage settings

--- .karma.js
+++ .karma.js
@@ -22,7 +22,7 @@ module.exports = function(config) {
       'straw-ios.js',
 
       // test
-      'straw-ios-spec.js',
+      'straw-ios-spec.js'
 
     ],
 
@@ -36,14 +36,21 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-    
+        'straw-ios.js': 'coverage'
     },
 
 
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+    reporters: ['progress', 'coverage'],
+
+
+    // coverage report settings
+    coverageReporter: {
+        type: 'lcov',
+        dir: 'coverage'
+    },
 
 
     // web server port
