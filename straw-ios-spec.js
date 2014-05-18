d7930b8a4af351145a1983eec53254e8906e446d
Yoshiya Hinosawa <stibium121@gmail.com>
2014-05-18 12:23:53 +0900

    Add a test of getRequestParams

--- straw-ios-spec.js
+++ straw-ios-spec.js
@@ -1,6 +1,6 @@
 // straw-ios-spec.js
 
-/* global describe, it, chai, sinon, straw */
+/* global describe, it, chai, straw */
 
 describe('straw.core', function () {
     'use strict';
@@ -12,6 +12,39 @@ describe('straw.core', function () {
         expect(straw).not.to.equal(null);
         expect(straw.core).not.to.equal(null);
 
+    });
+
+    describe('serviceCall', function () {
+    });
+
+
+    describe('getRequestParams', function () {
+
+        it('returns the request parameters of corresponding Straw Service call', function () {
+
+            straw.core.setLocation({});
+
+            var callId = straw.core.serviceCall('srv_x', 'meth_y', {abc: 123});
+
+            var params = straw.core.getRequestParams(callId);
+
+            expect(params).to.deep.equal({
+                service: 'srv_x',
+                method: 'meth_y',
+                params: {abc: 123},
+                callId: callId
+            });
+
+        });
+
+    });
+
+
+    describe('succeed', function () {
+    });
+
+
+    describe('fail', function () {
     });
 
 });
